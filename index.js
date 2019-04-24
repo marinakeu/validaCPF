ceeeea26a7d6bc9e4d13b93af9d6368eedfbb408
marinakeu <marina_keunecke@hotmail.com>
2019-04-24 17:41:31 -0300

    validator - implementa funções e testes de validação

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-function cpfValidator(char) {
-  return ['b', 'e', 'i', 'o', 'u'].indexOf(char) > -1;
-};
+function cpfValidator (cpfString) {
+  let cpf = cpfString.split('');
+  if (cpf.length < 11 || cpf.length > 11 || cpf.every(digit => digit === cpf[0])) {
+    return false;
+  } else if (digitValidator(cpf)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-module.exports = cpfValidator();
+function digitValidator (cpf) {
+  let nineDigitsCpf = cpf.slice(0, 9);
+  let firstDigit = getDigits(nineDigitsCpf, false);
+  let tenDigitsCpf = nineDigitsCpf;
+  tenDigitsCpf.push(firstDigit);
+  let secondDigit = getDigits(tenDigitsCpf, true);
+  if (firstDigit === parseInt(cpf[9]) && secondDigit === parseInt(cpf[10])) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getDigits (cpf, secondDigit) {
+  let number = secondDigit ? 11 : 10;
+  let multiply = cpf.map((element, index) => element * (number - index));
+  let sum = multiply.reduce((acc, element) => acc + element);
+  let digit = (sum % 11) < 2 ? 0 : 11 - (sum % 11);
+  return digit;
+}
+
+module.exports.cpfValidator = cpfValidator;
